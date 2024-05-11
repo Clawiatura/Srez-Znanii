bae966dbd510abec0af268aa2d75fa08b784bbf0
cemik - C1NB8 <aveal.cemik@gmail.com>
2024-05-11 21:20:05 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp60/Program.cs
@@ -0,0 +1,68 @@
+﻿
+//Console.WriteLine("Hello, World!");
+
+//Console.WriteLine("Введите ваше имя: ");
+//string name = Console.ReadLine();
+//Console.WriteLine(name);
+
+
+//Console.WriteLine("Введите первое число: ");
+//int a = int.Parse(Console.ReadLine());
+//Console.WriteLine("Введите второе число: ");
+//int b = int.Parse(Console.ReadLine());
+//int res = a + b;
+//Console.WriteLine(res);
+
+
+//Console.WriteLine("Введите число: ");
+//int a = int.Parse(Console.ReadLine());
+//if (a % 2 == 0) Console.WriteLine("Число четное");
+//else Console.WriteLine("Нечетное");
+
+//Console.WriteLine("Введите первое число: ");
+//int a = int.Parse(Console.ReadLine());
+//Console.WriteLine("Введите второе число: ");
+//int b = int.Parse(Console.ReadLine());
+//Console.WriteLine("Введите третье число: ");
+//int c = int.Parse(Console.ReadLine());
+
+//int maxNumber = a;
+//if (b > a && b>c) maxNumber = b;
+//else if (c > a && b > a && b<c) maxNumber = c;
+//Console.WriteLine(maxNumber);
+
+
+
+
+
+//Console.WriteLine("Введите число: ");
+//int x = int.Parse(Console.ReadLine());
+//long Factorial (int y)
+//{
+//    long f = 1;
+//    for (int i = 0; i <= y; i++) f *= i;
+//    return f;
+//}
+//Console.WriteLine(Factorial(x));
+
+
+
+
+//long summa (int x)
+//{
+//    long sum = 0;
+//    for (int i = 1; i <= x; i++) sum += i;
+//    return sum;
+
+//}
+//Console.WriteLine(summa(100));
+
+
+//Console.WriteLine("Введите текст ");
+//string text = Console.ReadLine();
+//Console.WriteLine((int)text.Length);
+
+Console.WriteLine("Введите число: ");
+int x = int.Parse(Console.ReadLine());
+if () Console.WriteLine("Число непростое");
+else Console.WriteLine("Число простое");
